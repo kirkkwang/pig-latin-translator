b1a5f770b55a8e3f409729743f1e5869c9df6570
kirkkwang <k3wang@gmail.com>
2022-01-26 11:24:54 -0800

    Updates

---
+++ precache-manifest.e2b46f03662fca6282ecf6989396404c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fffec355cf5f24330bb8874f20b8483b",
+    "url": "/pig-latin-translator/index.html"
+  },
+  {
+    "revision": "fda59f5287e1890d27f6",
+    "url": "/pig-latin-translator/static/css/main.8bc29cef.chunk.css"
+  },
+  {
+    "revision": "28838e893bedf6fee237",
+    "url": "/pig-latin-translator/static/js/2.67e2c9df.chunk.js"
+  },
+  {
+    "revision": "fda59f5287e1890d27f6",
+    "url": "/pig-latin-translator/static/js/main.dce296f7.chunk.js"
+  },
+  {
+    "revision": "5c47ed47dea2687e4d41",
+    "url": "/pig-latin-translator/static/js/runtime-main.7d105554.js"
+  },
+  {
+    "revision": "854c53ef4769d5e3595d69941cd5236a",
+    "url": "/pig-latin-translator/static/media/piggy.854c53ef.png"
+  }
+]);
